b5d0bc1c66e45883bdc33f9e85e95a8c0e677b26
Jesús García <js.garcia.fernandez@gmail.com>
2023-02-16 16:59:03 +0100

    DTO transfer class

---
+++ src/main/java/com/ironhack/finalProject/controller/dto/TransferDTO.java
@@ -0,0 +1,45 @@
+package com.ironhack.finalProject.controller.dto;
+
+import java.math.BigDecimal;
+
+public class TransferDTO {
+
+    private Long id;
+    private BigDecimal amount;
+    private Long issuingAccountId;
+    private Long receivingAccountId;
+
+
+    public TransferDTO() {
+    }
+
+    public TransferDTO(BigDecimal amount, Long issuingAccountId, Long receivingAccountId) {
+        this.amount = amount;
+        this.issuingAccountId = issuingAccountId;
+        this.receivingAccountId = receivingAccountId;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    public Long getIssuingAccountId() {
+        return issuingAccountId;
+    }
+
+    public void setIssuingAccountId(Long issuingAccountId) {
+        this.issuingAccountId = issuingAccountId;
+    }
+
+    public Long getReceivingAccountId() {
+        return receivingAccountId;
+    }
+
+    public void setReceivingAccountId(Long receivingAccountId) {
+        this.receivingAccountId = receivingAccountId;
+    }
+}
